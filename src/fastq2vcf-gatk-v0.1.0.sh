32f6d264dc3b461e85726104dfbc6ac5665bc0d9
deena-b <deenab7@gmail.com>
2019-08-09 17:30:07 -0700

    src/fastq2vcf-gatk-v0.1.0.sh edited thru bwa for filenames wLane RGinfo

--- src/fastq2vcf-gatk-v0.1.0.sh
+++ src/fastq2vcf-gatk-v0.1.0.sh
@@ -10,6 +10,9 @@ module load java/1.8.0.111      # language of gatk & picard-tools
 module load gatk/4.1.2.0        # includes picard tools
 module load bwa/0.7.8           # aligner
 
+## to run a script on hpc use `qsub filename.sh`
+## to check on the script's status: `qstat -u dalawson`
+## to stop a submitted project by job-ID number: `qdel job-ID-number`
 
 ######################
 ## create directories
@@ -17,7 +20,7 @@ module load bwa/0.7.8           # aligner
 
 ## before running this script
 ## make and move to this directory 
-## /dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/gen/nguyen_nc_2018/20190718-fastq2vcf/ind1/erroroutput/
+## /dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/gen/nguyen_nc_2018/20190809-fastq2vcf/ind1/erroroutput/
 ## run this script from this ^ directory
 ## so e & o files get deposited there
 
@@ -29,33 +32,40 @@ mkdir ../genomic/
 ## create variables
 ####################
 
-## create absolute path2data
-path2data='/dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/'
+## create absolute path2datadir
+path2datadir='/dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/'
 
-## create path2fastq holding directory
-path2fastq=${path2data}'raw/nguyen_nc_2018/ind1/'
+## create path2fastq holding directory variable
+path2fastq=${path2datadir}'raw/nguyen_nc_2018/ind1/'
+
+# create path to deposit data
+path2genomic=${path2datadir}'gen/nguyen_nc_2018/20190809-fastq2vcf/ind1/genomic/'
 
 ## create varibles for lists of files
-r1list=${path2data}'gen/nguyen_nc_2018/20190715-pairr1r2-renamed/ind1/r1_list.txt'
-r2list=${path2data}'gen/nguyen_nc_2018/20190715-pairr1r2-renamed/ind1/r2_list.txt'
+r1list=${path2datadir}'gen/nguyen_nc_2018/20190809-r1r2lists-i1-rename/r1list.txt'
+r2list=${path2datadir}'gen/nguyen_nc_2018/20190809-r1r2lists-i1-rename/r2list.txt'
 
 ## create a name variable
 ## note this name has '-R#.fastq.gz' extension included
-## e.g. L001-A02-CGTACTAG-GCGTAAGA-R1.fastq.gz
-namewfqgzext=`head -n $SGE_TASK_ID $r1list | tail -n 1 | cut -f1`
+## e.g. i1-lib001-L001-A01-TAAGGCGA-GCGTAAGA-R1.fastq.gz
+name1wext=`head -n $SGE_TASK_ID $r1list | tail -n 1 | cut -f1`
+name2wext=`head -n $SGE_TASK_ID $r2list | tail -n 1 | cut -f1`
 
 ## remove R# & ext from name
-## e.g. L001-A02-CGTACTAG-GCGTAAGA
-name=${namewfqgzext%'-R'[0-2].fastq.gz}
-platewell=${name:8}
-plate=${name:0:4}
-well=${name:5:3}
-barcodes=${name:9}
-bar1=${name:9:8}
-bar2=${name:18:8}
+## e.g. i1-lib001-L001-A01-TAAGGCGA-GCGTAAGA
+name=${name1wext%'-R'[1-2].fastq.gz}
+
+## assign variables to every piece of info in name
+ind=${name:0:2}
+lib=${name:3:6}
+lane=${name:10:4}
+well=${name:15:3}
+barcodes=${name:19}
+bar1=${name:19:8}
+bar2=${name:28}
 
 ## create output path
-path2output=${path2data}'gen/nguyen_nc_2018/20190718-fastq2vcf/ind1/genomic/'
+path2output=${path2datadir}'gen/nguyen_nc_2018/20190809-fastq2vcf/ind1/genomic/'
 
 ## create filename extenstion variables
 samext='.sam'
@@ -76,26 +86,44 @@ recal='recal-'
 proper='proper-'
 unpair='unpaired-'
 
-## assign a line of text to the readgroupinfo variable
+## define readgroupinfo variable
 ## bwa uses RGinfo to label things as normal or tumor
 ## A readgroup is a set of reads that were generated 
-## from a single run of a sequencing instrument.
-## Read as: ID = EGA, SM = name, PL = Illumina
-## with '\t' = tabs between them 
-## ID = readgroup ID (one for each illumina run)
-## Used platename, e.g.: "L001" (probably not correct)
-## SM = sample name
-## PU = {FLOWCELL_BARCODE}.{LANE}.{SAMPLE_BARCODE}
+    ## from a single run of a sequencing instrument.
+    ## '\t' inserts tabs between variables 
+## ID is readgroup ID (one for each illumina run)
+## ID in this case is a library
+## PU is {FLOWCELL_BARCODE}.{LANE}.{SAMPLE_BARCODE}
 ## PU takes precidence over ID for base recalibration, if present
 ## PU is not required by GATK
-## PL = platform/technology used to sequence
-readgroupinfo='@RG\tID:'${plate}'\tSM:'${name}'\tPL:Illumina'
+## SM is sample name
+## PL is platform/technology used to sequence
+readgroupinfo='@RG'\t'ID:'${lib}\t'PU:'${lib}.${lane}.${bar1}${bar2}\t'SM:'${name}\t'PL:Illumina'\t'LB:'${lib}
 
 
 ##########################
 ## create sub-directories
 ##########################
 
-## dirname = name - 
-dir=${}
+## make directory for each set of files
+mkdir $path2genomic$name
+
+## make path variable for file deposition
+path2output=$path2genomic$name'/'
+
+
+######################
+## gatk best practice
+######################
+
+## align reads to human_g1k_v37.fasta.gz
+bwa mem -M -t 32 -R $readgroupinfo \
+    $path2datadir'ref/broad/bundles/b37/human_g1k_v37.fasta.gz' \
+    $path2fastq$name1wext $path2fastq$name2wext \
+    > $path2output$name$samext
+
+
+
+
+
 
