032fedc46c2fa0b0d1f04120250aeb45adcb57db
deena-b <deenab7@gmail.com>
2019-08-15 15:49:21 -0700

    edit FastqToSam code: add backslash, quotes delete extra space

--- src/fastq2vcf-gatk-v0.1.0.sh
+++ src/fastq2vcf-gatk-v0.1.0.sh
@@ -145,9 +145,9 @@ path2output=$path2genomic$name'/'
 gatk FastqToSam \
     -F1 $path2fastq$name1wext \
     -F2 $path2fastq$name2wext \
-    -O  $path2ubams$unaligned$cell'-'$lane$bamext \
-    -SM $cell
-    -RG ${lib}.${lane}
+    -O $path2ubams$unaligned$cell'-'$lane$bamext \
+    -SM $cell \
+    -RG ${lib}'.'${lane}
 
 
 ## align reads to human reference
@@ -180,12 +180,14 @@ samtools view -b -q 20 \
     $path2output$aligned$name$samext chrM 
 
 
+
 ## merge bwa aligned, samtools filtered, bam files from the same cell
     ## select all filenames with lane='L001' & store as L1_list, select all filenames with 'L002' & store as L2_list
     ## edit the filenames in the two lists to remove L1 or L2, assign to variables s1 and s2, respectively
     ## if s1=s2, merge bam files with filenames that include L001 and L002
     ## is there a gatk merge bam files tool?
         ## MergeBamAlignment - merges aligned with unaligned to create unaligned bam
+            ## https://software.broadinstitute.org/gatk/documentation/tooldocs/current/picard_sam_MergeBamAlignment.php
         ## MergeSamFiles - use to combine SAM and/or BAM files from different runs (Lanes) (same as samtools merge)
             ## https://software.broadinstitute.org/gatk/documentation/tooldocs/4.1.3.0/picard_sam_MergeSamFiles.php
             ## https://broadinstitute.github.io/picard/command-line-overview.html#MergeSamFiles
