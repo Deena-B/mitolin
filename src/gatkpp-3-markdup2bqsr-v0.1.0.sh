256ec9cc1e42e9931c498b56cbd1bd6e3e6a015c
deena-b <deenab7@gmail.com>
2019-09-09 21:30:37 -0700

    write 1st draft of markdup

--- src/gatkpp-3-markdup2bqsr-v0.1.0.sh
+++ src/gatkpp-3-markdup2bqsr-v0.1.0.sh
@@ -12,12 +12,13 @@
 
 ## before running this script
 ## make and move to this directory 
-## /dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/gen/nguyen_nc_2018/20190809-fastq2vcf/ind1/erroroutput/
+## /dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/gen/nguyen_nc_2018/ \
+## 20190909-markdups-DRB/ind1/erroroutput/
 ## run this script from this ^ directory
 ## so e & o files get deposited there
 
 ## to run a script on hpc use `qsub path/to/filename.sh`
-    ## `qsub /dfs3/som/dalawson/drb/deepcelllineage/mitolin/src/uamgfil2bqsrecal-gatk-v0.1.0.sh`
+    ## `qsub /dfs3/som/dalawson/drb/deepcelllineage/mitolin/src/gatkpp-3-markdup2bqsr-v0.1.0.sh`
 ## to check on the script's status: `qstat -u dalawson`
 ## to stop a submitted project by job-ID number: `qdel job-ID-number`
 
@@ -28,40 +29,41 @@
 
 ## add packages to PATH
 module load java/1.8.0.111          # language of gatk & picard-tools
-# module load picard-tools/1.96     # for generating uBAM & merging uBAM w aBAM
 module load gatk/4.1.3.0            # includes picard tools
 module load samtools/1.9            # view (filter/convert) sort index
 
-# create path variables to access and deposit data 
+# create path variables to access data 
 path2datadir='/dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/'
-path2bam=${path2datadir}'gen/nguyen_nc_2018/20190809-fastq2uamgfil/ind1/genomic/uamgfil/'
-path2genomic=${path2datadir}'gen/nguyen_nc_2018/20190821-uamgfil2bqsrecal-DRB/ind1/genomic/'
-path2lanemerged=${path2genomic}'lanemerged/'
-path2cells=${path2genomic}'cells/'
-path2cell=${path2cells}${name}'/'
+# the path below is to samples that were only run in a single lane
+    # those listed in 'unpairedlist.txt'
+path2filuamgbam=${path2datadir}'gen/nguyen_nc_2018/20190809-gatkpp1-fastq2uamgfil-DRB/ind1/genomic/4-filuamg'
+# the path below is to lane-merged bam & bai files
+path2luamgfilbam=${path2datadir}'gen/nguyen_nc_2018/20190821-gatkpp2-lanesmerged-DRB/ind1/genomic/luamgfil/'
+# the path below has lists: unpairedlist.txt & lmglist.txt
+path2lists=${path2datadir}'gen/nguyen_nc_2018/20190906-celllist-DRB/'
 
-## make directories for each path above
+# create paths to deposit data
+path2genomic=${path2datadir}'gen/nguyen_nc_2018/20190909-markdups-DRB/ind1/genomic/'
+path2dupsmarked=${path2genomic}'dupsmarked/'
+path2metrics=${path2genomic}'dupmetrics/'
+
+## make directories for each 'deposit data' path above
 mkdir $path2genomic
-mkdir $path2lanemerged
-mkdir $path2cells
+mkdir $path2dupsmarked
+mkdir $path2dupmetrics
 
 ## create variables for lists of bam files
-l1list=${path2datadir}'gen/nguyen_nc_2018/20190820-pairlanelists-DRB/lane1list-paired.txt'
-l2list=${path2datadir}'gen/nguyen_nc_2018/20190820-pairlanelists-DRB/lane2list-paired.txt'
-singlerunlist=${path2datadir}'gen/nguyen_nc_2018/20190820-pairlanelists-DRB/unpairedlist.txt'
+unpairedlist=${path2lists}'unpairedlist.txt'
+lmglist=${path2datadir}'lmglist.txt'
 
 ## create a name variable
-## note this name has '-L00#.bam' extension included
-## e.g. filtered-uamerged-aligned-i1-lib001-A01-L001.bam
-name1wext=`head -n $SGE_TASK_ID $l1list | tail -n 1 | cut -f1`
-name2wext=`head -n $SGE_TASK_ID $l2list | tail -n 1 | cut -f1`
+## note this name has '.bam' extension included
+## e.g. i1-lib001-A01.bam
+namewext=`head -n $SGE_TASK_ID $lmglist | tail -n 1 | cut -f1`
 
-## remove L# & ext from name
-## e.g. name=filtered-uamerged-aligned-i1-lib001-A01
-name=${name1wext%'-L00'[1-2].bam}
-
-## make directory for individual cells
-mkdir $path2cell
+## remove ext from name
+## e.g. name=i1-lib001-A01
+name=${namewext%'.bam'}
 
 ## create filename extenstion variables
 samext='.sam'
@@ -72,44 +74,29 @@ table='table.csv'
 ptable='posttable.csv'
 vcfext='.vcf'
 
-## file prefixes
-unaligned='unaligned-'
-aligned='aligned-'
-filter='filtered-'
-uamerged='uamerged-'
-lanesmerged='lanesmerged-'
-sort='sorted-'
-dupm='dupmark-'
-
 
 #############################
 ## gatk pre-processing con't
 #############################
 
 
+## mark duplicates using gatk MarkDuplicates
+    ## https://software.broadinstitute.org/gatk/documentation/tooldocs/4.1.3.0/picard_sam_markduplicates_MarkDuplicates.php
+    ## All this does is mark duplicates with flags and generate metrics
+    ## You could choose to remove duplicates
+    ## I think marking dups is enough for them to be exculded from vcf
+    ## Inputs must be coordinate sorted
+
+# below currently only processes luamgfilbams
+gatk MarkDuplicates \
+    -I $path2luamgfilbam$name$bamext \
+    -O $path2dupsmarked$name$bamext \
+    -M $path2dupmetrics$name'.txt' \
+    --TAG_DUPLICATE_SET_MEMBERS true \
+    --CREATE_INDEX 
 
 
-
-
-
-
-
-
-
-
-
-
-## mark duplicates using gatk MarkDuplicatesSpark
-    ## https://software.broadinstitute.org/gatk/documentation/tooldocs/4.1.3.0/org_broadinstitute_hellbender_tools_spark_transforms_markduplicates_MarkDuplicatesSpark.php
-    ## 
-
-# gatk MarkDuplicates \
-#     -I $path2output$sort$filter$aligned$name$bamext \
-#     -O $path2output$dupm$sort$filter$aligned$name$bamext \
-#     -M $path2output'lib_complexity_metrics_from_markdups'$name'.txt'
-
-## next mark duplicates
-## check remove duplicates = True as default 
+## What goes next?
 
 
 
