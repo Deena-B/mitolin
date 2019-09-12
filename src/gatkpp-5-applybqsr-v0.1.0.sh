c2e2d5137e18cc1856ef900289bb1a8c1a2459f7
deena-b <deenab7@gmail.com>
2019-09-12 14:41:32 -0700

    add back $path2baserecaltable

--- src/gatkpp-5-applybqsr-v0.1.0.sh
+++ src/gatkpp-5-applybqsr-v0.1.0.sh
@@ -41,6 +41,7 @@ path2list=${path2bams}
 
 # create paths to deposit data
 path2genomic=${path2datadir}'gen/nguyen_nc_2018/20190911-bqsr-DRB/genomic/'
+path2baserecaltable=${path2genomic}'baserecaltable/'
 path2bqsrbams=${path2genomic}'bqsrbams/'
 
 ## make directories for each 'deposit data' path above
