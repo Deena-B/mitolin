4c220f0c481fbdf3968aba6716d10ac2a4cee46a
deena-b <deenab7@gmail.com>
2019-09-12 10:33:55 -0700

    use unzipped known-site vcfs

--- src/gatkpp-4-bqsr-v0.1.0.sh
+++ src/gatkpp-4-bqsr-v0.1.0.sh
@@ -81,9 +81,9 @@ vcfext='.vcf'
 gatk BaseRecalibrator \
    -I $path2bams$namewext \
    -R $path2datadir'ref/broad/bundles/b37/human_g1k_v37.fasta.gz' \
-   --known-sites $path2datadir'ref/broad/bundles/b37/dbsnp_138.b37.vcf.gz' \
-   --known-sites $path2datadir'ref/broad/bundles/b37/Mills_and_1000G_gold_standard.indels.b37.vcf.gz' \
-   --known-sites $path2datadir'ref/broad/bundles/b37/1000G_phase1.indels.b37.vcf.gz' \
+   --known-sites $path2datadir'ref/broad/bundles/b37/dbsnp_138.b37.vcf' \
+   --known-sites $path2datadir'ref/broad/bundles/b37/Mills_and_1000G_gold_standard.indels.b37.vcf' \
+   --known-sites $path2datadir'ref/broad/bundles/b37/1000G_phase1.indels.b37.vcf' \
    -O $path2baserecaltable$name'.table'
 
 
