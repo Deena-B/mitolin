e1090894301b30035b04381d68ce941b161f6fc4
deena-b <deenab7@gmail.com>
2019-10-08 15:14:44 -0700

    add list of hg38 files wget.sh

---
+++ src/hg38-v0-broad-references-wget.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+URLS=(
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G.phase3.integrated.sites_only.no_MATCHED_REV.hg38.vcf
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G.phase3.integrated.sites_only.no_MATCHED_REV.hg38.vcf.idx
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G_omni2.5.hg38.vcf.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G_omni2.5.hg38.vcf.gz.tbi
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G_phase1.snps.high_confidence.hg38.vcf.gz.tbi
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G_phase3_v4_20130502.sites.hg38.vcf
+    https://storage.googleapis.com/broad-references/hg38/v0/1000G_phase3_v4_20130502.sites.hg38.vcf.idx
+    https://storage.googleapis.com/broad-references/hg38/v0/Axiom_Exome_Plus.genotypes.all_populations.poly.hg38.vcf.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/Axiom_Exome_Plus.genotypes.all_populations.poly.hg38.vcf.gz.tbi
+    https://storage.googleapis.com/broad-references/hg38/v0/GRCh38.primary_assembly.genome.fa
+    https://storage.googleapis.com/broad-references/hg38/v0/GRCh38_gencode.v27.refFlat.txt
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.UD
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.V
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.bed
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.exome_calling_regions.v1.README.sh
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.exome_calling_regions.v1.UD
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.exome_calling_regions.v1.bed
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.exome_calling_regions.v1.mu
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.contam.mu
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.dbsnp138.vcf.idx
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.dict
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.alt
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.amb
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.ann
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.bwt
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.pac
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.64.sa
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.amb
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.ann
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.bwt
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.fai
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.pac
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.fasta.sa
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.haplotype_database.txt
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.known_indels.vcf.gz.tbi
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.ref_cache.tar.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.tile_db_header.vcf
+    https://storage.googleapis.com/broad-references/hg38/v0/Homo_sapiens_assembly38.vid
+    https://storage.googleapis.com/broad-references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/Mills_and_1000G_gold_standard.indels.hg38.vcf.gz.tbi
+    https://storage.googleapis.com/broad-references/hg38/v0/README
+    https://storage.googleapis.com/broad-references/hg38/v0/WholeGenomeShotgunContam.vcf
+    https://storage.googleapis.com/broad-references/hg38/v0/WholeGenomeShotgunContam.vcf.idx
+    https://storage.googleapis.com/broad-references/hg38/v0/autosomes-1kg-minusNA12878-ALL.vcf
+    https://storage.googleapis.com/broad-references/hg38/v0/exome_calling_regions.v1.interval_list
+    https://storage.googleapis.com/broad-references/hg38/v0/exome_calling_regions.v1.interval_list.deprecated.DSDEGP-652
+    https://storage.googleapis.com/broad-references/hg38/v0/exome_evaluation_regions.v1.interval_list
+    https://storage.googleapis.com/broad-references/hg38/v0/gencode.v27.primary_assembly.annotation.gtf
+    https://storage.googleapis.com/broad-references/hg38/v0/gencode.v27.rRNA.interval_list
+    https://storage.googleapis.com/broad-references/hg38/v0/gencode_v27_primary.tar
+    https://storage.googleapis.com/broad-references/hg38/v0/gencode_v27_trans_rsem.tar.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/genome_snp_tran.tar.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/hapmap_3.3.hg38.vcf.gz
+    https://storage.googleapis.com/broad-references/hg38/v0/hapmap_3.3.hg38.vcf.gz.tbi
+    https://storage.googleapis.com/broad-references/hg38/v0/hg38.even.handcurated.20k.chr20_test.intervals
+    https://storage.googleapis.com/broad-references/hg38/v0/hg38.even.handcurated.20k.intervals
+    https://storage.googleapis.com/broad-references/hg38/v0/wgs_calling_regions.hg38.interval_list
+    https://storage.googleapis.com/broad-references/hg38/v0/wgs_coverage_regions.hg38.interval_list
+    https://storage.googleapis.com/broad-references/hg38/v0/wgs_evaluation_regions.hg38.interval_list
+    https://storage.googleapis.com/broad-references/hg38/v0/wgs_metrics_intervals.interval_list
+)
+
+for u in "${URLS[@]}"; do
+    wget "$u"
+    echo 'sleeping for 2 seconds'
+    sleep 2
+done
