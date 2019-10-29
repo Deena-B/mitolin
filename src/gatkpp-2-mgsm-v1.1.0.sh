595783ee83822757ba64db37fb4a29de2e0f5194
deena-b <deenab7@gmail.com>
2019-10-29 14:53:26 -0700

    edit paths to use hg38-mapped bams

--- src/gatkpp-2-mgsm-v1.1.0.sh
+++ src/gatkpp-2-mgsm-v1.1.0.sh
@@ -3,7 +3,7 @@
 #$ -ckpt restart
 #$ -q som,pub64,free64,asom
 #$ -pe make 64
-#$ -t 1-118
+#$ -t 2-4
 
 
 #############
@@ -12,12 +12,12 @@
 
 ## before running this script
 ## make and move to this directory 
-## /dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/gen/nguyen_nc_2018/20190809-fastq2vcf/ind1/erroroutput/
+## /dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/gen/nguyen_nc_2018/20191029-mgsm/erroroutput/
 ## run this script from this ^ directory
 ## so e & o files get deposited there
 
 ## to run a script on hpc use `qsub path/to/filename.sh`
-    ## `qsub /dfs3/som/dalawson/drb/deepcelllineage/mitolin/src/uamgfil2bqsrecal-gatk-v0.1.0.sh`
+    ## `qsub /dfs3/som/dalawson/drb/deepcelllineage/mitolin/src/gatkpp-2-mgsm-v1.1.0.sh`
 ## to check on the script's status: `qstat -u dalawson`
 ## to stop a submitted project by job-ID number: `qdel job-ID-number`
 
@@ -28,37 +28,41 @@
 
 ## add packages to PATH
 module load java/1.8.0.111          # language of gatk & picard-tools
-# module load picard-tools/1.96     # for generating uBAM & merging uBAM w aBAM
 module load gatk/4.1.3.0            # includes picard tools
-module load samtools/1.9            # view (filter/convert) sort index
 
 # create path variables to access and deposit data 
 path2datadir='/dfs3/som/dalawson/drb/deepcelllineage/mitolin/data/'
-path2bam=${path2datadir}'gen/nguyen_nc_2018/20190809-gatkpp1-fastq2uamgfil-DRB/ind1/genomic/4-filuamg/'
-path2genomic=${path2datadir}'gen/nguyen_nc_2018/20190821-uamgfil2bqsrecal-DRB/ind1/genomic/'
-path2luamgfil=${path2genomic}'luamgfil/'
+path2gen_nguyen18=${path2datadir}'gen/nguyen_nc_1018/'
+path2bam=${path2gen_nguyen18}'20191025-fastq2ummg/output/3-ummg/'
+path2output=${path2gen_nguyen18}'20191029-mgsm/output'
+path2luamg=${path2output}'luamg/'
 
 ## make directories for each path above
-mkdir $path2genomic
-mkdir $path2luamgfil
+mkdir $path2output
+mkdir $path2luamg
+
+## make .keep files so folders are tracked by git
+touch $path2output/.keep
+touch $path2luamg/.keep 
+
+## create path variables to access lists of bam files
+path2lists=${path2gen_nguyen18}'20190906-pairlanelists-DRB/'
 
 ## create variables for lists of bam files
-l1list=${path2datadir}'gen/nguyen_nc_2018/20190820-pairlanelists-DRB/lane1list-paired.txt'
-l2list=${path2datadir}'gen/nguyen_nc_2018/20190820-pairlanelists-DRB/lane2list-paired.txt'
-singlerunlist=${path2datadir}'gen/nguyen_nc_2018/20190820-pairlanelists-DRB/unpairedlist.txt'
+## generated using ipynb *pairl1l2*
+l1list=${path2lists}'lane1list-paired.txt'
+l2list=${path2lists}'lane2list-paired.txt'
+singlerunlist=${path2lists}'unpairedlist.txt'
 
 ## create a name variable
 ## note this name has '-L00#.bam' extension included
-## e.g. filtered-uamerged-aligned-i1-lib001-A01-L001.bam
+## e.g. i1-lib001-A01-L001.bam
 name1wext=`head -n $SGE_TASK_ID $l1list | tail -n 1 | cut -f1`
 name2wext=`head -n $SGE_TASK_ID $l2list | tail -n 1 | cut -f1`
 
-## remove L# & ext from name
-## e.g. name=filtered-uamerged-aligned-i1-lib001-A01
+## remove Lane # & ext from name
+## e.g. i1-lib001-A01
 name=${name1wext%'-L00'[1-2].bam}
-
-## create filename extenstion variables
-bamext='.bam'
 
 
 #############################
@@ -77,10 +81,10 @@ bamext='.bam'
 gatk MergeSamFiles \
     -I ${path2bam}${name1wext} \
     -I ${path2bam}${name2wext} \
-    -O ${path2luamgfil}${name}${bamext} \
+    -O ${path2luamg}${name}'.bam' \
     --CREATE_INDEX true
 
-## next gatkpp3-markdup2bqsr
+## next gatkpp3-markdup
 
 
 
